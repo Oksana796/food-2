233fa7775b40a9bfff4511dc6986d229e3773ca6
Oksana <ksenia79460@gmail.com>
2022-05-18 12:45:33 +0100

    POST request by formData

--- js/script.js
+++ js/script.js
@@ -214,3 +214,50 @@ new MenuCard(
   430,
   ".menu .container"
 ).render();
+
+// Forms, POST request
+
+const forms = document.querySelectorAll("form");
+
+const message = {
+  loading: "Loading",
+  success: "Thanks, we will contact you soon.",
+  failure: "Oops, something det wrong...",
+};
+
+forms.forEach((item) => {
+  postData(item);
+});
+
+function postData(form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const statusMessage = document.createElement("div");
+    statusMessage.classList.add("status");
+    statusMessage.textContent = message.loading;
+    form.append(statusMessage);
+
+    const request = new XMLHttpRequest();
+    request.open("POST", "server.php");
+
+    //request.setRequestHeader("Content-type", "multipart/form-data");
+    //(FormData does not need this method, but JSON need.)
+    const formData = new FormData(form);
+
+    request.send(formData);
+
+    request.addEventListener("load", () => {
+      if (request.status === 200) {
+        console.log(request.response);
+        statusMessage.textContent = message.success;
+        form.reset();
+        setTimeout(() => {
+          statusMessage.remove();
+        }, 2000);
+      } else {
+        statusMessage.textContent = message.failure;
+      }
+    });
+  });
+}

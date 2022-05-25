16e0327e0da008cf4a66566bb916e2e9a66a1879
Oksana <ksenia79460@gmail.com>
2022-05-25 09:57:06 +0100

    add axios for get request

--- js/script.js
+++ js/script.js
@@ -145,7 +145,7 @@ function showModalByScroll() {
 }
 window.addEventListener("scroll", showModalByScroll);
 
-// Card  (menu)
+// create Card (menu) with Classes
 
 class MenuCard {
   constructor(src, alt, title, descr, price, parentSelector, ...classes) {
@@ -194,11 +194,45 @@ const getResult = async (url, data) => {
   return await res.json();
 };
 
-getResult("http://localhost:3000/menu").then((data) => {
-  data.forEach(({ img, altimg, title, descr, price }) => {
-    new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+//render cards dynamically on page from server:
+// getResult("http://localhost:3000/menu")
+//   .then((data) => {
+//     data.forEach(({ img, altimg, title, descr, price }) => {
+//       new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+//   });
+// });
+
+axios.get("http://localhost:3000/menu")
+  .then((data) => {
+    data.data.forEach(({ img, altimg, title, descr, price }) => {
+      new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+    });
   });
-});
+
+// create cards without Classes and reproduce them on page:
+
+// getResource('http://localhost:3000/menu')
+//     .then(data => createCard(data));
+
+// function createCard(data) {
+//     data.forEach(({img, altimg, title, descr, price}) => {
+//         const element = document.createElement('div');
+
+//         element.classList.add("menu__item");
+
+//         element.innerHTML = `
+//             <img src=${img} alt=${altimg}>
+//             <h3 class="menu__item-subtitle">${title}</h3>
+//             <div class="menu__item-descr">${descr}</div>
+//             <div class="menu__item-divider"></div>
+//             <div class="menu__item-price">
+//                 <div class="menu__item-cost">Цена:</div>
+//                 <div class="menu__item-total"><span>${price}</span> грн/день</div>
+//             </div>
+//         `;
+//         document.querySelector(".menu .container").append(element);
+//     });
+// }
 
 // Forms, POST request
 
